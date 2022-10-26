77723e00541a3a1e21acbc2fe45441f9a4eb786d
Gekky-Pro-grammer <malpekaratharv@gmail.com>
2022-10-26 15:42:20 +0530

    changes

--- services/addon_service.py
+++ services/addon_service.py
@@ -1,5 +1,4 @@
-from models.addon import Addon
-
+from models.add_on import AddOns
 def service_get_addons_data():
     addonObj = AddOns.objects()
     return  list(map(lambda x: x.to_json(), addonObj))[0]
