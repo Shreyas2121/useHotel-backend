9f9ba86e959f59ee93c4657a2ca7001577425f73
Shreyas Rasaikar <shreyas.rasaikar@infuse.it>
2022-10-27 16:57:15 +0530

    fixed

--- services/coupon_service.py
+++ services/coupon_service.py
@@ -7,17 +7,17 @@ def create_coupon_service():
     discount_res = request.get_json()['discount']
     coupon = Coupon(code=coupon_code, discount_percentage=discount_res)
     coupon.save()
-    return Response(status=200, mimetype='application/json', response='{"message": "Coupon Created"}')
-
+    return jsonify({'message': 'Coupon Created'}), 200
 def get_coupons_service():
     couponObj = Coupon.objects()
-    return Response(status=200, mimetype='application/json', response=jsonify({'coupons': [coupon.to_json() for coupon in couponObj]}))
+    return jsonify({'coupons': [coupon.to_json() for coupon in couponObj]}), 200
 
 
 def check_coupon_service():
+    print(request.get_json())
     coupon_res = request.get_json()['coupon']
     couponObj = Coupon.objects(code=coupon_res)
     if couponObj:
-        return Response(status=200, mimetype='application/json', response=jsonify(couponObj[0].discount_percentage))
+        return jsonify(couponObj[0].discount_percentage), 200
     else:
-        return Response(status=400, mimetype='application/json', response={"message": "Invalid Coupon"})
+        return "Invalid Coupon", 400
