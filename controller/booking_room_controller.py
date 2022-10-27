52f874ba8063e9c904b7d12dac55f919ea0d2837
ayaanshaikh0187 <ayaanshaikhtks@gmail.com>
2022-10-27 12:59:45 +0530

    Worked on PR comments for cancel booking and cancel hall and renaming

--- controller/booking_room_controller.py
+++ controller/booking_room_controller.py
@@ -11,8 +11,8 @@ def check_booking_room_controller():
     return booking_room_service.service_check_room_availability()
 
 
-def delete_room_booking():
-    return cancelling_room_service.service_delete_booking()
+def delete_room_booking_controller():
+    return cancelling_room_service.delete_booking_service()
 
 def get_bookings_by_email_controller():
-    return cancelling_room_service.service_get_bookings_by_email()
+    return cancelling_room_service.get_bookings_by_email_service()
