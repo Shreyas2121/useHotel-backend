9a3b52132f91a346e8b7a983679ba226c5bc26d7
imtiyaz <imtiyazkhan458@gmail.com>
2022-10-28 17:36:58 +0530

    minor refactoring

--- routes/booking_hall_route.py
+++ routes/booking_hall_route.py
@@ -2,30 +2,30 @@ from flask import Blueprint, request
 
 from controller.booking_hall_controller import get_hall_bookings_controller, book_hall_controller, get_hall_availability_controller, delete_hall_booking_controller, get_bookings_by_email_controller
 
-booking_hall_route = Blueprint('booking_hall_route', __name__)
+hall_booking_bp = Blueprint('booking_hall_route', __name__)
 
 
-@booking_hall_route.route('/booking/hall', methods=['GET'])
+@hall_booking_bp.route('/booking/hall', methods=['GET'])
 def get_hall_bookings():
     return get_hall_bookings_controller()
 
 
-@booking_hall_route.route('/booking/hall', methods=['POST'])
+@hall_booking_bp.route('/booking/hall', methods=['POST'])
 def book_hall():
     return book_hall_controller()
 
 
-@booking_hall_route.route('/booking/hall/availability', methods=['GET'])
+@hall_booking_bp.route('/booking/hall/availability', methods=['GET'])
 def get_halls_availability():
     return get_hall_availability_controller()
 
 
-@booking_hall_route.route('/booking/hall/<string:_id>', methods=['DELETE'])
+@hall_booking_bp.route('/booking/hall/<string:_id>', methods=['DELETE'])
 def delete_hall_booking(_id):
     return delete_hall_booking_controller(_id)
 
 
-@booking_hall_route.route('/booking/hall/<string:email>', methods=['GET'])
+@hall_booking_bp.route('/booking/hall/<string:email>', methods=['GET'])
 def get_hall_bookings_by_email(email):
     print(email)
     return get_bookings_by_email_controller(email)
--- routes/coupon_route.py
+++ routes/coupon_route.py
@@ -2,19 +2,19 @@ from flask import Blueprint
 
 from controller.coupon_controller import create_coupon_controller, get_coupons_controller, validate_coupon_controller
 
-coupons_route = Blueprint('coupons_route', __name__)
+coupons_bp = Blueprint('coupons_route', __name__)
 
 
-@coupons_route.route('/coupons', methods=['POST'])
+@coupons_bp.route('/coupons', methods=['POST'])
 def create_coupon():
     return create_coupon_controller()
 
 
-@coupons_route.route('/coupons', methods=['GET'])
+@coupons_bp.route('/coupons', methods=['GET'])
 def route_get():
     return get_coupons_controller()
 
 
-@coupons_route.route('/coupon/validate', methods=['POST'])
+@coupons_bp.route('/coupon/validate', methods=['POST'])
 def route_check():
     return validate_coupon_controller()
