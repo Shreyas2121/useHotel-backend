2d93636056c72550d1742f8922eb25cdfd338a83
PranavBagve <107304934+PranavBagve@users.noreply.github.com>
2022-11-01 11:43:29 +0530

    fixed hall tests

--- services/booking_room_service.py
+++ services/booking_room_service.py
@@ -82,13 +82,10 @@ def get_room_availability_service():
 
 def delete_booking_service(id):
     booking = BookingRoom.objects().get(pk=id)
-    print('test')
-    print(booking)
     booking.delete()
     return jsonify({"message": "Room Booking Deleted"}), 200
 
 
 def get_bookings_by_email_service(email):
     bookings = BookingRoom.objects().filter(email=email)
-    print(bookings)
     return list(map(lambda x: x.to_json(), bookings)), 200
